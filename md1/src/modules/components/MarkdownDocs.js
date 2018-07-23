60687804b59b6cb9ed46cc94c473285c2d65c493
Michael Angerman <stormasm@gmail.com>
2018-07-23 13:29:48 -0700

    mw

--- md1/src/modules/components/MarkdownDocs.js
+++ md1/src/modules/components/MarkdownDocs.js
@@ -5,16 +5,14 @@ import warning from 'warning';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import MarkdownElement from '@material-ui/docs/MarkdownElement';
-import Head from 'docs/src/modules/components/Head';
 import AppContent from './AppContent';
-import Demo from 'docs/src/modules/components/Demo';
-import Carbon from 'docs/src/modules/components/Carbon';
+
 import {
   getHeaders,
   getContents,
   getTitle,
   getDescription,
-} from 'docs/src/modules/utils/parseMarkdown';
+} from './../utils/parseMarkdown';
 
 const styles = theme => ({
   root: {
@@ -36,58 +34,15 @@ const demoRegexp = /^"demo": "(.*)"/;
 const SOURCE_CODE_ROOT_URL = 'https://github.com/mui-org/material-ui/tree/master';
 
 function MarkdownDocs(props, context) {
-  const { classes, demos, disableCarbon, markdown, markdownLocation: markdownLocationProp } = props;
+  const { classes, demos, markdown } = props;
   const contents = getContents(markdown);
   const headers = getHeaders(markdown);
 
-  let markdownLocation = markdownLocationProp || context.activePage.pathname;
-
-  if (!markdownLocationProp) {
-    const token = markdownLocation.split('/');
-    token.push(token[token.length - 1]);
-    markdownLocation = token.join('/');
-
-    if (headers.filename) {
-      markdownLocation = headers.filename;
-    } else {
-      markdownLocation = `/docs/src/pages${markdownLocation}.md`;
-    }
-  }
-
-  const section = markdownLocation.split('/')[4];
+  const section = 'lab';
 
   return (
     <AppContent className={classes.root}>
-      <Head
-        title={`${headers.title || getTitle(markdown)} - Material-UI`}
-        description={getDescription(markdown)}
-      />
-      <div className={classes.header}>
-        <Button component="a" href={`${SOURCE_CODE_ROOT_URL}${markdownLocation}`}>
-          {'Edit this page'}
-        </Button>
-      </div>
-      {disableCarbon ? null : <Carbon key={markdownLocation} />}
       {contents.map((content, index) => {
-        const match = content.match(demoRegexp);
-
-        if (match && demos) {
-          const demoOptions = JSON.parse(`{${content}}`);
-
-          const name = demoOptions.demo;
-          warning(demos && demos[name], `Missing demo: ${name}.`);
-          return (
-            <Demo
-              key={content}
-              js={demos[name].js}
-              raw={demos[name].raw}
-              index={index}
-              demoOptions={demoOptions}
-              githubLocation={`${SOURCE_CODE_ROOT_URL}/docs/src/${name}`}
-            />
-          );
-        }
-
         return <MarkdownElement className={classes.markdownElement} key={content} text={content} />;
       })}
       {headers.components.length > 0 ? (
@@ -114,21 +69,10 @@ ${headers.components
 MarkdownDocs.propTypes = {
   classes: PropTypes.object.isRequired,
   demos: PropTypes.object,
-  disableCarbon: PropTypes.bool,
   markdown: PropTypes.string.isRequired,
   // You can define the direction location of the markdown file.
   // Otherwise, we try to determine it with an heuristic.
   markdownLocation: PropTypes.string,
-};
-
-MarkdownDocs.defaultProps = {
-  disableCarbon: false,
-};
-
-MarkdownDocs.contextTypes = {
-  activePage: PropTypes.shape({
-    pathname: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default withStyles(styles)(MarkdownDocs);
