0796306b7146b360297039748f64f6773a8fc198
Michael Angerman <stormasm@gmail.com>
2018-07-24 12:36:34 -0700

    mw

--- md2/src/containers/DataViewWrapper.js
+++ md2/src/containers/DataViewWrapper.js
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import DataView from "./DataView";
 
+const template =
+  "https://raw.githubusercontent.com/stormasm/ghdata/master/markdown/data1/";
+
 class DataViewWrapper extends React.Component {
   constructor(props) {
     super(props);
@@ -17,16 +20,52 @@ class DataViewWrapper extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
+
     this.setState({ isLoading: true });
     this.setState({ viewName: nextProps.view });
+
+    const url = template + nextProps.view + '.md';
+    console.log('1', url);
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          throw new Error(
+            "Sorry, something went wrong in the DataViewWrapper part I"
+          );
+        }
+      })
+      .then(data => this.setState({ data, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
   }
 
   componentDidMount() {
-    this.setState({ isLoading: false });
+    this.setState({ isLoading: true });
     this.setState({ viewName: this.props.view });
+
+    const url = template + this.state.viewName + '.md';
+    console.log('2', url);
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          throw new Error(
+            "Sorry, something went wrong in the DataViewWrapper part II"
+          );
+        }
+      })
+      .then(data => this.setState({ data, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
   }
 
   render() {
+
+    console.log(this.state.data);
+
     if (this.state.error) {
       return <p>{this.state.error.message}</p>;
     }
@@ -37,7 +76,7 @@ class DataViewWrapper extends React.Component {
 
     return (
       <div>
-        <DataView viewName={this.state.viewName} />
+        <DataView viewName={this.state.viewName} markdown={'cool'}/>
       </div>
     );
   }
